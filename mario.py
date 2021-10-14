1b486ab11b46c8ef0e77bab79f604f47e4f3d4c0
truebird2201 <oy7405@naver.com>
2021-10-14 21:09:12 +0900

    메인화면 제작
    메인화면 제작
    space바를 누르면 다음화면으로 전환

--- mario.py
+++ mario.py
@@ -5,6 +5,13 @@ from math import *
 
 WIDTH, HEIGHT = 1000, 800
 
+running = True
+game = 0
+fly_frame = 0
+frame = 0
+main_frame = False
+main_move = False
+
 x, y = WIDTH // 2, HEIGHT // 2
 
 class player:
@@ -13,9 +20,9 @@ class player:
     global x
     global y
     global frame
+    global main_frame
     dir = 0
     dir2 = 1
-    frame = 0
     gravity = 5
     jumpPower = 40
     jumpTime = 0
@@ -41,6 +48,7 @@ class player:
         global x
         global y
         global frame
+        global main_frame
 
         events = get_events()
         for event in events:
@@ -48,6 +56,9 @@ class player:
             if game == 0:  # 대기 화면
                 if event.type == SDL_QUIT:
                     running = False
+                elif event.type == SDL_KEYDOWN:  # 키 다운
+                    if event.key == SDLK_SPACE:  # 스페이스
+                        game = 1
 
             elif game == 1:  # 게임 진행
                 if event.type == SDL_QUIT:  # 끄기
@@ -93,6 +104,7 @@ class player:
         global x
         global y
         global frame
+        global main_frame
 
         self.left = x - 30
         self.right = x + 30
@@ -100,8 +112,15 @@ class player:
         self.bottom = y + 30
 
         if game == 0:
+            main_back.clip_draw(0, 0, 1000, 800, 500, 400)
+            main_sonic.clip_draw(0, 0, 400, 340, 150, 200 + main_frame)
+            Title.clip_draw(0, 0, 1000, 300, 500, 600 - main_frame,800,200)
+            name.clip_draw(0, 0, 1000, 300, 500, 750 - main_frame, 400, 120)
+            if (int)(main_frame % 10) != 0:
+                press.clip_draw(0, 0, 800, 300, 500, 400, 400, 150)
             update_canvas()
             self.handle_events(player)
+
         elif game == 1:
             self.left = x - 20
             self.right = x + 20
@@ -141,34 +160,32 @@ class player:
             elif self.dir == 0 and self.dir2 == -1:       # 마지막이 왼쪽이였던 멈춤
                 sonic_sprite.clip_composite_draw(int(frame) * 40, 420, 40, 40, 0, 'h', x, y, 60, 60)
 
-            update_canvas()
-
-
-            self.handle_events(player)
-
-            if self.fast and self.dir != 0:                           # 대시 on
+            if self.fast and self.dir != 0:  # 대시 on
                 x += self.dir * 16
                 delay(0.04)
-            else:                                           # 대시 off
+            else:  # 대시 off
                 x += self.dir * 10
                 delay(0.04)
 
-
+        update_canvas()
+        self.handle_events(player)
 
 open_canvas(WIDTH, HEIGHT)
+
 
 sonic_sprite = load_image('sonic.png')
 coin = load_image('coin.png')
 red_coin = load_image('red_coin.png')
 fly = load_image('fly_monster.png')
-
-running = True
-game = 1
-fly_frame = 0
-frame = 0
+main_back = load_image('main_back.png')
+main_sonic = load_image('main_sonic.png')
+name = load_image('LeeSeoYeon.png')
+Title = load_image('SuperSonic.png')
+press = load_image('press.png')
 
 while running:
     clear_canvas()
+
     player.draw(player)
 
     if player.dir == 0:  # 프레임
@@ -176,5 +193,18 @@ while running:
     else:
         frame = (frame + 1) % 8
     fly_frame = (fly_frame + 1) % 16
+
+    if main_move == True:
+        main_frame = main_frame + 0.2
+    if main_frame > 10:
+        main_move = False
+    if main_move == False:
+        main_frame = main_frame - 0.2
+    if main_frame < 0:
+        main_move = True
+
+
+    else:
+        pass
 
 close_canvas()
