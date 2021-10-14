11c4cdeb8a3202e50da6b01b7a6783d44d71d15b
truebird2201 <oy7405@naver.com>
2021-10-14 13:57:05 +0900

    점프 및 더블점프 구현
    점프와 더블점프를 구현

--- mario.py
+++ mario.py
@@ -8,18 +8,26 @@ WIDTH, HEIGHT = 1020, 800
 x, y = WIDTH // 2, HEIGHT // 2
 
 class player:
-    Jumping = False
-    fast = False
+    global running
+    global game
+    global x
+    global y
+    global frame
     dir = 0
     dir2 = 1
     frame = 0
-    gravity = 9.8
-    jumpPower = 15.0
-    jumpTime = 2
+    gravity = 5
+    jumpPower = 40
+    jumpTime = 0
     left = x - 20
     right = x + 20
     top = y - 20
     bottom = y + 20
+    savey = 0
+    savey2 = 0
+    jumpcount = 2
+    Jumping = False
+    fast = False
 
     def __init__(self, x, y):
         self.x = x
@@ -27,13 +35,12 @@ class player:
 
 
     def handle_events(self):
+
         global running
         global game
-
-        self.dir
-        self.dir2
-        self.fast
-
+        global x
+        global y
+        global frame
 
         events = get_events()
         for event in events:
@@ -56,7 +63,18 @@ class player:
                     elif event.key == SDLK_ESCAPE:  # ESC
                         self.running = False
                     elif event.key == SDLK_SPACE:  # 스페이스
-                        self.Jumping = True
+                        if self.jumpcount == 2:
+                            self.savey = y
+                            self.savey2 = y
+                            self.Jumping = True
+                            self.jumpcount -= 1
+
+                        elif self.jumpcount == 1:
+                            self.jumpTime = 0
+                            self.savey2 = y
+                            self.Jumping = True
+                            self.jumpcount -= 1
+
                     elif event.key == SDLK_LSHIFT or event.key == SDLK_RSHIFT:  # 쉬프트
                         self.fast = True
 
@@ -69,15 +87,13 @@ class player:
                         self.fast = False
 
     def draw(self):
+
         global running
         global game
         global x
         global y
         global frame
 
-        self.dir
-        self.dir2
-        self.fast
         self.left = x - 20
         self.right = x + 20
         self.top = y - 20
@@ -92,23 +108,38 @@ class player:
             self.top = y - 20
             self.left = x - 20
 
+            if self.Jumping:
+                y = (self.jumpTime * self.jumpTime * (-self.gravity) / 2) + (self.jumpTime * self.jumpPower) + self.savey2
+                self.jumpTime += 1
+                if y < self.savey:
+                    y = self.savey
+                    self.Jumping = False
+                    self.jumpTime = 0.0
+                    self.jumpcount = 2
+
             if self.dir == 1:                        # 오른쪽
                 if self.fast:                       # 대시
-                    sonic_sprite.clip_draw(int(self.frame) * 40, 380, 40, 40, x, y)
+                    sonic_sprite.clip_draw(int(frame) * 40, 380, 40, 40, x, y)
                 else:
-                    sonic_sprite.clip_draw(int(self.frame) * 40, 460, 40, 40, x, y)
+                    if self.Jumping:
+                        sonic_sprite.clip_draw(int(frame) * 40, 340, 40, 40, x, y)
+                    else:
+                        sonic_sprite.clip_draw(int(frame) * 40, 460, 40, 40, x, y)
 
             elif self.dir == -1:                     # 왼쪽
                 if self.fast:                       # 대시
-                    sonic_sprite.clip_composite_draw(int(self.frame) * 40, 380, 40, 40, 0, 'h', x, y, 40, 40)
+                    sonic_sprite.clip_composite_draw(int(frame) * 40, 380, 40, 40, 0, 'h', x, y, 40, 40)
                 else:
-                    sonic_sprite.clip_composite_draw(int(self.frame) * 40, 460, 40, 40, 0, 'h', x, y, 40, 40)
+                    if self.Jumping:
+                        sonic_sprite.clip_composite_draw(int(frame) * 40, 340, 40, 40, 0, 'h', x, y, 40, 40)
+                    else:
+                        sonic_sprite.clip_composite_draw(int(frame) * 40, 460, 40, 40, 0, 'h', x, y, 40, 40)
 
             elif self.dir == 0 and self.dir2 == 1:        # 마지막이 오른쪽이였던 멈춤
-                sonic_sprite.clip_draw(int(self.frame) * 40, 420, 40, 40, x, y)
+                sonic_sprite.clip_draw(int(frame) * 40, 420, 40, 40, x, y)
 
             elif self.dir == 0 and self.dir2 == -1:       # 마지막이 왼쪽이였던 멈춤
-                sonic_sprite.clip_composite_draw(int(self.frame) * 40, 420, 40, 40, 0, 'h', x, y, 40, 40)
+                sonic_sprite.clip_composite_draw(int(frame) * 40, 420, 40, 40, 0, 'h', x, y, 40, 40)
 
             update_canvas()
 
@@ -116,7 +147,7 @@ class player:
             self.handle_events(player)
 
             if self.fast and self.dir != 0:                           # 대시 on
-                x += self.dir * 10
+                x += self.dir * 12
                 delay(0.04)
             else:                                           # 대시 off
                 x += self.dir * 6
@@ -141,9 +172,9 @@ while running:
     player.draw(player)
 
     if player.dir == 0:  # 프레임
-        player.frame = (player.frame + 0.5) % 8
+        frame = (frame + 0.5) % 8
     else:
-        player.frame = (player.frame + 1) % 8
+        frame = (frame + 1) % 8
     fly_frame = (fly_frame + 1) % 16
 
 close_canvas()
