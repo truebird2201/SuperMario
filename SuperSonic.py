6be3099422eacef200de0d60681c90d664245b28
truebird2201 <oy7405@naver.com>
2021-10-20 21:24:27 +0900

    Revert "Revert "framework 1차 나누기""
    This reverts commit a3735c99fd5d552d5e869826a499d2ba0cf96953.

---
+++ SuperSonic.py
@@ -0,0 +1,8 @@
+import game_framework
+import pico2d
+
+import Start_state
+
+pico2d.open_canvas()
+game_framework.run(Start_state)
+pico2d.close_canvas()
---
+++ Title_state.py
@@ -0,0 +1,43 @@
+import game_framework
+import Select_state
+from pico2d import *
+
+
+name = "TitleState"
+image = None
+
+
+def enter():
+    global image
+    image = load_image('Mario_credit.png')
+
+def exit():
+    global image
+    del(image)
+
+def handle_events():
+    events = get_events()
+    for event in events:
+        if event.type == SDL_QUIT:
+            game_framework.quit()
+        else:
+            if (event.type, event.key) == (SDL_KEYDOWN, SDLK_ESCAPE):
+                game_framework.quit()
+            elif (event.type, event.key) == (SDL_KEYDOWN, SDLK_SPACE):
+                game_framework.change_state(Select_state)
+
+def draw():
+    clear_canvas()
+    image.draw(512, 300, 1024, 600)
+    update_canvas()
+
+def update():
+    pass
+
+
+def pause():
+    pass
+
+
+def resume():
+    pass
